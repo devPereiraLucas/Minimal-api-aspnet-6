960fd181bf98a86d3be3ebe54e0241e532b11aa0
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-06-29 15:31:30 -0300

    Adicionar arquivos de projeto.

---
+++ Models/fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalApi.Models
+{
+    public class fornecedor
+    {
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public string Cnpj { get; set; }
+        public string? Documento { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
